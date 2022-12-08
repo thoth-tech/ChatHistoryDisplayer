a530367c74bd02bf0200caa5d89c11970717c02e
Ricky Dodd <rickydodd@users.noreply.github.com>
2022-12-09 02:12:12 +1100

    fix: 520 rubocop offenses
    437 of the 520 offenses were auto-correctable, but 83 offenses had to be
    fixed manually.

--- emulator/main.rb
+++ emulator/main.rb
@@ -1,136 +1,120 @@
-#Gems 
-require "rubygems"
-require "git"
-require "dir"
-require "logger"
-require "sinatra"
-require "sinatra/cross_origin"
-require "json"
+# frozen_string_literal: true
 
-#User defined classes/Gemsruby
-require "./generator.rb"
-require "./util.rb"
+# Gems
+require 'rubygems'
+require 'git'
+require 'dir'
+require 'logger'
+require 'sinatra'
+require 'sinatra/cross_origin'
+require 'json'
+
+# User defined classes/Gemsruby
+require './generator'
+require './util'
 
 # HTTP Response boilerplate
 set :bind, '0.0.0.0'
 
 configure do
-    enable :cross_origin
+  enable :cross_origin
 end
 
 before do
-    content_type :json
-    response.headers['Access-Control-Allow-Origin'] = '*'
+  content_type :json
+  response.headers['Access-Control-Allow-Origin'] = '*'
 end
 
 # REST API METHODS HERE
-get '/test' do
-    # Have coded it to return true. Will use this as our positive outcome scenario
-    outcome = GitGenerator.test() 
-
-    # Determine what response the server gives based on that outcome
-    case outcome
-    # Positive result
-    when true
-        Response.generic("200", "Positive outcome")
-    # Negative result
-    when false
-        Response.generic("500", "Negative outcome")
-    # Unknown result
-    else 
-        Response.generic("501", "Unknown outcome")
-    end
-
-end
-
 get '/init/:uid' do |uid|
-    resp = GitGenerator.init(uid)
-    case resp
-    when true
-        Response.generic("200", "Created a git repo for #{uid}")
-    when false 
-        Response.generic("401", "Repo already exists for #{uid}")
-    else 
-        Response.generic("501", "Unknown error. Check server logs")
-    end
+  GitGenerator.init
+  resp = GitGenerator.create_user_repo(uid)
+  case resp
+  when true
+    Response.generic('200', "Created a git repo for #{uid}")
+  when false
+    Response.generic('401', "Repo already exists for #{uid}")
+  else
+    Response.generic('501', 'Unknown error. Check server logs')
+  end
 end
 
 get '/:uid' do |uid|
-    puts 'retrieving Git repo'
-    GitGenerator.retrieve(uid)
+  puts 'Does user repo exist?'
+  GitGenerator.user_repo_exists?(uid)
 end
 
 # Submit a file for a user
 post '/:uid' do |uid|
-    puts 'posting a file to ' + uid + " repo"
-    request.body.rewind
-    data = JSON.parse(request.body.read)
-    GitGenerator.postTo(uid, data["fileName"], data["file"], data["commitMsg"])
+  puts "posting a file to #{uid} repo"
+  request.body.rewind
+  data = JSON.parse(request.body.read)
+  GitGenerator.postTo(uid, data['fileName'], data['file'], data['commitMsg'])
 end
 
 # Delete a file for a user
 delete '/:uid' do |uid|
-    puts 'Deleting repo for ' + uid
-    # Delete code
-    Response.generic("200", "Deleted file")
+  puts "Deleting repo for #{uid}"
+  # Delete code
+  Response.generic('200', 'Deleted file')
 end
 
 get '/diff/:uid/:file' do |uid, file|
-    resp = JSON.parse(GitGenerator.getDif(uid, file))
-    
-    case resp["Found"]
-    when true
-        Response.generic("200", resp["sha"])
-    when false
-        Response.generic("404", "null")
-    end
+  resp = JSON.parse(GitGenerator.getDif(uid, file))
+
+  case resp['Found']
+  when true
+    Response.generic('200', resp['sha'])
+  when false
+    Response.generic('404', 'null')
+  end
 end
 
 post '/requiredFiles/:uid' do |uid|
-    request.body.rewind
-    data = JSON.parse(request.body.read)
+  request.body.rewind
+  data = JSON.parse(request.body.read)
 
-    resp = GitGenerator.setRequiredFiles(uid, data)
-    case resp
-    when true
-        Response.generic("200", "Updated required files for #{uid}")
-    when false
-        Response.generic("500", "Something went wrong")
-    end
+  resp = GitGenerator.set_required_files(uid, data)
+  case resp
+  when true
+    Response.generic('200', "Updated required files for #{uid}")
+  when false
+    Response.generic('500', 'Something went wrong')
+  end
 end
 
 get '/requiredFiles/:uid' do |uid|
-    resp = GitGenerator.getRequiredFiles(uid)
-    case resp 
-    when false
-        Response.generic("404", "Repo doesn't exist for #{uid}")
-    else
-        Response.generic("200", resp)
-    end
+  resp = GitGenerator.get_required_files(uid)
+  case resp
+  when false
+    Response.generic('404', "Repo doesn't exist for #{uid}")
+  else
+    Response.generic('200', resp)
+  end
 end
 
 get '/checkUploadStatus/:uid' do |uid|
-    resp = GitGenerator.checkUploadStatus(uid)
-    case resp
-    when false
-        Response.generic("404", "Repo doesn't exist for #{uid}")
-    else
-        Response.generic("200", resp)
-    end
+  resp = GitGenerator.required_files_exist?(uid)
+  case resp
+  when false
+    Response.generic('404', "The required files do not exist for UID: #{uid}.")
+  else
+    Response.generic('200', resp)
+  end
 end
-    
+
 get '/log/:uid' do |uid|
-    resp = GitGenerator.getLog(uid)
-    case resp
-    when false
-        Response.generic("404", "Repo doesn't exist for #{uid}")
-    else
-        Response.generic("200", resp)
-    end
+  resp = GitGenerator.get_log(uid)
+  case resp
+  when false
+    Response.generic('404', "Repo doesn't exist for #{uid}")
+  else
+    Response.generic('200', resp)
+  end
 end
 
 # HTTP Response boilerplate
-options "*" do
-    response.headers["ALLOW"] = "GET, PUT, POST, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+options '*' do
+  response.headers['ALLOW'] = 'GET, PUT, POST, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
 end
--- emulator/util.rb
+++ emulator/util.rb
@@ -1,18 +1,18 @@
-# Utility functions used by our server 
+# frozen_string_literal: true
+
+# Utility functions used by our server
 class Response
+  # Generic server response
+  def self.generic(code, message)
+    { Code: code, Message: message }.to_json
+  end
 
-    #Generic server response 
-    def self.generic(code, message)
-        return {Code: code, Message: message}.to_json
-    end
-    #Response containing the outocme out a dif search with an attached SHA if one was found
-    def self.diffOutcome(found, sha)
-        return {Found: found, sha: sha}.to_json
-    end
+  # Response containing the outocme out a dif search with an attached SHA if one was found
+  def self.diff_outcome(found, sha)
+    { Found: found, sha: sha }.to_json
+  end
 
-    def self.genericText(code,message)
-        return {Code: code, Message: message}
-    end
-
+  def self.generic_text(code, message)
+    { Code: code, Message: message }
+  end
 end
-
