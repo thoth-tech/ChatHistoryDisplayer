f41ead8e6c70a3a9eb91df44b3f646d01905b6e7
Ricky Dodd <rickydodd@users.noreply.github.com>
2022-12-10 01:52:46 +1100

    fix: change capybara config
    `bundle exec rspec` wasn't working with `docker compose up` (where
    docker compose is ran at proj root), as the capybara config was
    attempting to start up a second instance of the server. By not having
    capybara start the server, `bundle exec rspec` can be used with docker
    compose seemlessly.

--- emulator/spec/integration_spec.rb
+++ emulator/spec/integration_spec.rb
@@ -3,7 +3,10 @@
 require 'capybara/rspec'
 require './main'
 
+# configuring capybara to not start the server (as `docker compose up` should be ran at root),
+# set the app_host to the react web app, default driver to headless chrome, and the server port
 Capybara.configure do |config|
+  config.run_server = false
   config.app_host = 'http://localhost:3000'
   config.app = Sinatra::Base
   config.default_driver = :selenium_chrome_headless
